f4dba18acde72b52d3f8f516823f5b2619f78859
Joannalyt <lyt307166987@hotmail.com>
2015-03-05 00:22:13 -0800

    added blog content, mobile menu adjustment
    lots of changes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Visage/js/mobileContent.js
@@ -0,0 +1,19 @@
+$(".document").ready(
+	function(){
+		console.log("hamburger.js loaded successfully");
+		var width = $(window).width();
+		console.log("page width: " + width);
+		
+		if(width <= 960){
+			$("nav").insertAfter("header");
+			$("#siteSearch").hide();
+			$("nav ul ul").css("display","none");
+			$("header").css("padding-left","40%");
+			$("nav").css("margin-bottom", "1em");
+			
+		}
+		
+		
+	}
+);
+
